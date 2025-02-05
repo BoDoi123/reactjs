1f0c67812aef39fd95a6e4a4dc369f549ce3b63a
Chu Long <chulong_t65@hus.edu.vn>
2025-02-05 23:54:57 +0700

    ReactJS - Review Array JS

---
+++ review_array_js/js/main.js
@@ -0,0 +1,38 @@
+const traiCay = ["cam", "xoai", "nho", "tao"];
+
+console.log(typeof traiCay);
+console.log(traiCay);
+
+// Truy cap cac phan tu cua mang
+console.log(traiCay[0]);
+console.log(traiCay[1]);
+console.log(traiCay[2]);
+console.log(traiCay[3]);
+
+// 2. Mang long nhau
+const mang = [1, "hello", true, null, undefined, [1, 2, 3]];
+
+// Truy cap mang long nhau
+console.log(mang[5][0]);
+console.log(mang[5][1]);
+
+// 3. Mot so phuong thuc thuong dung
+// 3.1 push(): Them phan tu
+traiCay.push("dua");
+
+// 3.2 map(): map sẽ duyệt qua từng phần tử của mảng, tạo mảng mới từ mảng cũ
+const so = [1, 2, 3, 4, 5];
+console.log(so);
+
+const so2 = so.map((item) => item * 2);
+console.log(so2);
+
+// Bien doi phan tu cua mang thanh doi tuong moi
+const so3 = so.map((item) => ({ so: item }));
+
+// 3.3 findIndex()
+const kq = traiCay.findIndex((item) => {
+    return item === "cam";
+});
+
+console.log(kq);
